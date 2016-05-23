44bdf5c876f9bd864e2b53f14df5053e9585e73c
Liliane Bezerra Lima <liliane.b.lima@outlook.com>
2016-05-23 09:37:28 -0300

    Inicio

---
+++ GameShowApp/GameShowApp/ModelManager.swift
@@ -0,0 +1,20 @@
+//
+//  ModelManager.swift
+//  GameShowApp
+//
+//  Created by Liliane Bezerra Lima on 23/05/16.
+//  Copyright © 2016 Gabriel Alberto. All rights reserved.
+//
+
+import UIKit
+
+class ModelManager {
+    let sharedInstance = ModelManager()
+    
+    
+    func getQuestions() {
+        
+    }
+    
+    
+}
